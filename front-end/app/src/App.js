4abb23789c575d53925fa2690f0a5e2f94cf59a8
Connor Mccoy <jmccoy7@clemson.edu>
2022-04-11 20:18:16 +0000

    Updated App.js

--- front-end/app/src/App.js
+++ front-end/app/src/App.js
@@ -9,11 +9,6 @@ import Wishlist from "./Components/pages/Wishlist.js";
 import Dev from "./Components/pages/Dev.js";
 import Catalog from "./Components/Catalog/Catalog.js";
 import Listing from "./Components/Catalog/listing_details.js";
-import Sponsor_Profile from "./Components/pages/Sponsor_Profile.js";
-import Driver_Sponsor_Dashboard from "./Components/pages/Driver_Sponsor_Dashboard.js";
-import Driver_Management from "./Components/pages/Driver_Management.js";
-import Apply_To_Sponsor from "./Components/pages/Apply_To_Sponsor.js";
-import Admin_Profile from "./Components/pages/Admin_Profile.js";
 
 function App() {
   return (
@@ -23,17 +18,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/pages/Application" element={<Application />} />
           <Route path="/pages/Driver_Profile" element={<Profile />} />
-          <Route path="/pages/Sponsor_Profile" element={<Sponsor_Profile />} />
-          <Route path="/pages/Admin_Profile" element={<Admin_Profile />} />
           <Route path="/main/points" element={<Points />} />
           <Route path="/pages/Login" element={<Login />} />
           <Route path="/pages/Wishlist" element={<Wishlist />} />
           <Route path="/pages/Dev" element={<Dev />} />
           <Route path="/Catalog/Catalog" element={<Catalog />} />
           <Route path="/Catalog/Listing" element={<Listing />} />
-          <Route path="/pages/Driver_Sponsor_Dashboard" element={<Driver_Sponsor_Dashboard />} />
-          <Route path="/pages/Driver_Management" element={<Driver_Management />} />
-          <Route path="/pages/Apply_To_Sponsor" element={<Apply_To_Sponsor />} />
         </Routes>
       </div>
     </div>
