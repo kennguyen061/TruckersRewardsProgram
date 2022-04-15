366cd62e8092ac1e7fb3c9728cf8a9d25ad1a091
Zach Howes <zhowes14@icloud.com>
2022-04-14 23:03:10 -0400

    fixed navbar problem on home

--- front-end/app/src/Components/pages/Home.js
+++ front-end/app/src/Components/pages/Home.js
@@ -1,19 +1,17 @@
-//import { Link, Route } from "react-router-dom";
 import "./Home.css";
 import NewNav from "../UI/HomeNav";
-import DriverNav from "../UI/DriverNav";
 import Hero from "../Hero/Hero";
 import People from "../People/People";
 import WeStand from "../WeStand/WeStand";
 import Footer from "../Footer/Footer";
-import React, { useState, useEffect } from'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 toast.warn("This text demonstrates the annoucement feature");
 function Home() {
   return (
     <div>
-      <DriverNav />
+      <NewNav />
       <Hero />
       <People />
       <WeStand />
@@ -29,8 +27,7 @@ function Home() {
         draggable
         pauseOnHover
       />
-      </div>
-      
+    </div>
   );
 }
 export default Home;
