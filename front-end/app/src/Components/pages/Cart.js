08c76ad760455fc54da5f8ec3ef737dfbd3796ed
Richardw2019 <rgarci3@clemson.edu>
2022-04-19 23:13:42 -0400

    working on cart

--- front-end/app/src/Components/pages/Cart.js
+++ front-end/app/src/Components/pages/Cart.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 import DriverNav from "../UI/DriverNav";
 import "./Cart.css";
 import "react-toastify/dist/ReactToastify.css";
-
-//Retreive cart from the logged in driver
-//call / route
-
-
+import { toast } from "react-toastify";
+toast.configure();
 
 export default function Cart() {
-  const [cart, setCart] = useState("");
+
+  const [cart, setCart] = useState([]);
 
   const role = window.localStorage.getItem("role");
   const id = window.localStorage.getItem("id");
@@ -17,8 +15,8 @@ export default function Cart() {
   const url = new URL ("http://18.235.52.212:8000/cart/")
 
   url.searchParams.append("UID", id)
+  //will need a call to find the SID soon
   url.searchParams.append("SID", 1)
-
 
   const fetchData = () => {
     fetch(url, {
@@ -28,16 +26,52 @@ export default function Cart() {
       .then(response => response.json())
       .then((data) => {
         setCart(data)
-      });
-
-      // .then(err => {console.log(err);});
+      })
   };
 
-  console.log(cart)
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-      fetchData();
-    }, []);
+
+  const remove = (item) => () => {
+
+    // const url = new URL ("http://18.235.52.212:8000/cart/remove")
+
+    // url.searchParams.append("UID", id)
+    // //will need a call to find the SID soon
+    // url.searchParams.append("SID", 1)
+    // url.searchParams.append("ItemID", item.ItemID)
+
+    // fetch(url, {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    // })
+
+
+
+  }
+
+  const increase_qty = (item) => () => {
+    const url = new URL ("http://18.235.52.212:8000/cart/increasequantity")
+
+    url.searchParams.append("UID", id)
+    //will need a call to find the SID soon
+    url.searchParams.append("SID", 1)
+    url.searchParams.append("ItemID", item.ItemID)
+
+
+
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+
+    toast("Increased Quantity")
+  }
+
+  const decrease_qty = (item) => () => {
+  }
 
     
   return (
@@ -45,28 +79,46 @@ export default function Cart() {
       <DriverNav />
 
       <div className="cart">
-        {/* iterate through cart items for driver  */}
-        <div>
-          <div>
-            <button className="button_1" style={{ float: "right" }}>
-              {" "}
-              +{" "}
-            </button>
-            {/* add total quantity in cart (call /increasequantity route) */}
+        {(cart).map((cart) => (
+        <div className="item"  style={{ border: "2px solid black", marginTop:10 }}>
+          <center>
+            <h2 style={{marginTop: 5}}>{cart.ItemName}</h2>
+            <h3>Inventory Quantity: {cart.Availability}</h3>
+            <h3 style={{color: "red", marginBottom:10}}> Cost: {cart.Price}</h3>
+          </center>
 
-            <button className="button_1" style={{ float: "right" }}>
+
+          <div className ="quantity">   
+            <div style={{marginLeft:50, float:"right"}}>
+                <button className="button_2" style={{alignItems: "right"}} onClick={remove(cart)}>
+                  {" "}
+                  Remove{" "}
+                </button>
+              </div>
+
+            <button className="button_1" style={{ float: "left" }}>
               {" "}
               -{" "}
             </button>
-          </div>
-          {/* when user clicks this button, it will remove the item the cart (call /remove) */}
-          <button className="button_1" style={{ float: "right" }}>
-            {" "}
-            Save Catalog{" "}
-          </button>
-        </div>
 
-        <button className="button_1" style={{ float: "right" }}>
+           <b> Qty: {cart.Quantity}</b> 
+
+           <button className="button_1" style={{ float: "right" }} onClick={increase_qty(cart)}>
+              {" "}
+              +{" "}
+            </button>
+
+
+           
+         
+          </div>
+
+          {/* when user clicks this button, it will remove the item the cart (call /remove) */}
+         
+        </div>
+         ))}
+
+        <button className="button_1" style={{marginTop: 30}}>
           {" "}
           Checkout{" "}
         </button>
