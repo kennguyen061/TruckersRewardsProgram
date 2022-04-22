2e0c881115a9ebc337c57bee68cb2cf12631d5bc
jmccoy7@clemson.edu <jmccoy7@clemson.edu>
2022-04-22 10:16:49 -0400

    Error page now dynamically selects navbar

--- front-end/app/src/Components/pages/Error.js
+++ front-end/app/src/Components/pages/Error.js
@@ -1,5 +1,9 @@
 import "./Error.css";
 import NewNav from "../UI/HomeNav";
+import DriverNav from "../UI/DriverNav";
+import SponsorNav from "../UI/SponsorNav";
+import AdminNav from "../UI/AdminNav";
+import { Link, Route, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 import Video from "../../assets/broll_5.mp4";
@@ -7,9 +11,29 @@ import Video from "../../assets/broll_5.mp4";
 //current videos are 1080p try some 720 idk they are too long
 
 function Error() {
+  const role = window.localStorage.getItem("role");
+  const id = window.localStorage.getItem("id");
+  const navigate = useNavigate();
+  let nav;
+
+  const url = new URL("http://18.235.52.212:8000/account/read");
+
+  url.searchParams.append("role", role);
+  url.searchParams.append("id", id);
+
+  if (role === "DRIVER") {
+    nav = <DriverNav />;
+  } else if (role === "SPONSOR") {
+    nav = <SponsorNav />;
+  } else if (role === "ADMIN") {
+    nav = <AdminNav />;
+  } else {
+    nav = <NewNav />;
+  }
+
   return (
     <div>
-      <NewNav />
+      {nav}
     <div className="error">
       <video autoPlay loop muted id="video">
         <source src={Video} type="video/mp4" />
