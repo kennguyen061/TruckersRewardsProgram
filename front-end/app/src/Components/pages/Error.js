d88db77c25883e01622ca7d24e699517bb16c62c
Zach Howes <zhowes14@icloud.com>
2022-04-22 11:28:52 -0400

    small changes

--- front-end/app/src/Components/pages/Error.js
+++ front-end/app/src/Components/pages/Error.js
@@ -14,12 +14,7 @@ function Error() {
   const role = window.localStorage.getItem("role");
   const id = window.localStorage.getItem("id");
   const navigate = useNavigate();
-  let nav;
-
-  const url = new URL("http://18.235.52.212:8000/account/read");
-
-  url.searchParams.append("role", role);
-  url.searchParams.append("id", id);
+  let nav = <NewNav />;
 
   if (role === "DRIVER") {
     nav = <DriverNav />;
@@ -27,21 +22,23 @@ function Error() {
     nav = <SponsorNav />;
   } else if (role === "ADMIN") {
     nav = <AdminNav />;
-  } else {
+  } /*else {
     nav = <NewNav />;
-  }
+  }*/
 
   return (
     <div>
       {nav}
-    <div className="error">
-      <video autoPlay loop muted id="video">
-        <source src={Video} type="video/mp4" />
-      </video>
-      <div className="overlay"></div>
+      <div className="error">
+        <video autoPlay loop muted id="video">
+          <source src={Video} type="video/mp4" />
+        </video>
+        <div className="overlay"></div>
         <div className="content">
           <h1>Looks like you got lost while driving</h1>
-          <h2>Please try to login again or contact your systems administrator!</h2>
+          <h2>
+            Please try to login again or contact your systems administrator!
+          </h2>
         </div>
       </div>
     </div>
