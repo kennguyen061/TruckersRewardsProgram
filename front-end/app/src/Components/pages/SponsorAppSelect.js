f58ffd6170bd6109d21b93bdfa4d407daa119331
Zach Howes <zhowes14@icloud.com>
2022-04-23 13:49:03 -0400

    fixed endpoint needed

--- front-end/app/src/Components/pages/SponsorAppSelect.js
+++ front-end/app/src/Components/pages/SponsorAppSelect.js
@@ -8,7 +8,14 @@ function SponsorAppSelect(props) {
     <div>
       <DriverNav />
       <div className="spacer"></div>
-      <div></div>
+
+      <div className="content">
+        <div>
+          <h1>New Sponsor Applicants</h1>
+          <h2> Please accept and reject all new applicants</h2>
+        </div>
+        <div className="app-box">{/* for each new applicant mke div*/}</div>
+      </div>
 
       <Footer />
     </div>
