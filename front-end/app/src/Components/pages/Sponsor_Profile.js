96b5d89b0665c58f3f1e0df0e07e237c03a9cc83
jmccoy7@clemson.edu <jmccoy7@clemson.edu>
2022-04-24 17:01:14 -0400

    Sponsor Profile style is now up to date with latest Profile Template

--- front-end/app/src/Components/pages/Sponsor_Profile.js
+++ front-end/app/src/Components/pages/Sponsor_Profile.js
@@ -5,142 +5,155 @@ import "./Sponsor_Profile.css";
 
 function Sponsor_Profile() {
   const role = window.localStorage.getItem("role");
-    const id = window.localStorage.getItem("id");
-    
-    const [firstName, setFirstName] = useState("name");
-    const [lastName, setLastName] = useState("lastName");
-    const [email, setEmail] = useState("email");
-    const [address, setAddress] = useState("address");
-    const [phoneNumber, setPhoneNumber] = useState("phoneNumber");
-
-    const firstNameHandler = (event) => {
-      setFirstName(event.target.value);
+  const id = window.localStorage.getItem("id");
+  const [firstName, setFirstName] = useState("name");
+  const [lastName, setLastName] = useState("lastName");
+  const [firstNameUpdated, setFirstNameUp] = useState("name");
+  const [lastNameUpdated, setLastNameUp] = useState("lastName");
+  const [email, setEmail] = useState("email");
+  const [address, setAddress] = useState("address");
+  const [phoneNumber, setPhoneNumber] = useState("phoneNumber");
+  
+  const firstNameHandler = (event) => {
+    setFirstName(event.target.value);
+  };
+  
+  const lastNameHandler = (event) => {
+    setLastName(event.target.value);
   };
 
-  const lastNameHandler = (event) => {
-      setLastName(event.target.value);
+  const firstNameUpdateHandler = (event) => {
+    setFirstNameUp(event.target.value);
+  };
+
+  const lastNameUpdateHandler = (event) => {
+    setLastNameUp(event.target.value);
   };
 
   const emailHandler = (event) => {
-      setEmail(event.target.value);
+    setEmail(event.target.value);
   };
 
   const addressHandler = (event) => {
-      setAddress(event.target.value);
+    setAddress(event.target.value);
   };
 
   const phoneNumberHandler = (event) => {
-      setPhoneNumber(event.target.value);
+    setPhoneNumber(event.target.value);
   };
+
   const updateHandler = (event) => {
+    event.preventDefault();
     const url = "http://18.235.52.212:8000/account/update";
     const userInfo = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        street: address,
-        phoneNum: phoneNumber,
-        role: role,
-        id: id
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      street: address,
+      phoneNum: phoneNumber,
+      role: role,
+      id: id,
     };
 
     fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userInfo),
-    }).then((response) => {
-        console.log(response);
-    }).catch((error) => {
-        console.log("Error: ", error);
-    });
-}
-
-const url = new URL("http://18.235.52.212:8000/account/read");
-url.searchParams.append("role", role);
-url.searchParams.append("id", id);
-
-useEffect(() => {
-    fetch(url, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userInfo),
     })
-        .then((response) => response.json())
-        .then((sponsor) => {
-            setFirstName(sponsor.firstName);
-            setLastName(sponsor.lastName);
-            setEmail(sponsor.email);
-            setAddress(sponsor.address);
-            setPhoneNumber(sponsor.phoneNumber);
-        });
-}, [firstName]);
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
 
-return (
-  <div className="card">
-    <SponsorNav />
-    <div className="Sponsor_Profile">
-      <form onSubmit={updateHandler}>
-        <div>
-          <div className="header">
-            <label className="Login-Header">
-              Welcome Back {firstName} {lastName} !
-            </label>
-            <hr className="line" />
-            <h5>Fill in all text bubbles to update personal information</h5>
-          </div>
+  const url = new URL("http://18.235.52.212:8000/account/read");
+  url.searchParams.append("role", role);
+  url.searchParams.append("id", id);
+
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((sponsor) => {
+        setFirstName(sponsor.firstName);
+        setLastName(sponsor.lastName);
+        setEmail(sponsor.email);
+        setAddress(sponsor.address);
+        setPhoneNumber(sponsor.phoneNumber);
+      });
+  }, [firstName]);
+
+  return (
+    <div className="card">
+      <SponsorNav />
+      <div className="Sponsor_Profile">
+        <form onSubmit={updateHandler}>
           <div>
-            <div className="pii-field">
-              <label className="Name">First Name</label>
-              <input
-                type={"text"}
-                placeholder={firstName}
-                onChange={firstNameHandler}
-              />
+            <div className="header">
+              <label className="Login-Header">
+                Welcome Back {firstName} {lastName} !
+              </label>
+              <hr className="line" />
+              <h5>Fill in all text bubbles to update personal information</h5>
             </div>
-            <div className="pii-field">
-              <label className="Name">Last Name</label>
-              <input
-                type={"text"}
-                placeholder={lastName}
-                onChange={lastNameHandler}
-              />
-            </div>
-            <div className="pii-field">
-              <label className="Email">Email</label>
-              <input
-                type={"text"}
-                placeholder={email}
-                onChange={emailHandler}
-              />
-            </div>
-            <div className="pii-field">
-              <label className="Address">Address</label>
-              <input
-                type={"text"}
-                placeholder={address}
-                onChange={addressHandler}
-              />
-            </div>
-            <div className="pii-field">
-              <label className="Phone">Phone Number</label>
-              <input
-                type={"text"}
-                placeholder={phoneNumber}
-                onChange={phoneNumberHandler}
-              />
-            </div>
-            <div className="button">
-              <button type="submit" onClick={updateHandler}>
-                Update
-              </button>
+            <div>
+              <div className="pii-field">
+                <label className="Name">First Name</label>
+                <input
+                  type={"text"}
+                  placeholder={firstName}
+                  onChange={firstNameUpdateHandler}
+                />
+              </div>
+              <div className="pii-field">
+                <label className="Name">Last Name</label>
+                <input
+                  type={"text"}
+                  placeholder={lastName}
+                  onChange={lastNameUpdateHandler}
+                />
+              </div>
+              <div className="pii-field">
+                <label className="Email">Email</label>
+                <input
+                  type={"text"}
+                  placeholder={email}
+                  onChange={emailHandler}
+                />
+              </div>
+              <div className="pii-field">
+                <label className="Address">Address</label>
+                <input
+                  type={"text"}
+                  placeholder={address}
+                  onChange={addressHandler}
+                />
+              </div>
+              <div className="pii-field">
+                <label className="Phone">Phone Number</label>
+                <input
+                  type={"text"}
+                  placeholder={phoneNumber}
+                  onChange={phoneNumberHandler}
+                />
+              </div>
+              <div className="button">
+                <button type="submit" onClick={updateHandler}>
+                  Update
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
-
-    <Footer />
-  </div>
-);
-}
-
-export default Sponsor_Profile;
+        </form>
+      </div>
+  
+        <Footer />
+      </div>
+    );
+  }
+  export default Sponsor_Profile;
+  
