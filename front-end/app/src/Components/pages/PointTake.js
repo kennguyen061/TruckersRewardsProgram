6cd249ba5b8ac5b6eb13b51db36980e28b3dd948
Dylan Faulk <faulk@g.clemson.edu>
2022-04-23 23:05:59 -0400

    finally done

--- front-end/app/src/Components/pages/PointTake.js
+++ front-end/app/src/Components/pages/PointTake.js
@@ -4,27 +4,17 @@ import Footer from "../Footer/Footer";
 import SponsorNav from "../UI/SponsorNav";
 
 const PointTake = () => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredPointChange, setEnteredPointChange] = useState(0);
-  const [driversID, setDriversID] = useState(0);
-  const [driversCurrPoints, setDriversCurrPoints] = useState(0);
-  const [useThisID, setUseID] = useState(0);
-  const [useThisPoints, setUsePoints] = useState(0);
-  const sid = 1; //window.localStorage.getItem("sid");
-
   const SubmitHandler = async (event) => {
     event.preventDefault();
-    const dataSet = {
-      Email: enteredEmail,
-      Changa: enteredPointChange,
-    };
 
-    console.log(dataSet);
     const url1 = new URL(
       "http://18.235.52.212:8000/drivermgt/getDriverByEmail"
     ); //params are email
 
-    url1.searchParams.append("Email", enteredEmail);
+    url1.searchParams.append(
+      "Email",
+      window.localStorage.getItem("DRIVEREMAIL")
+    );
 
     const url2 = new URL("http://18.235.52.212:8000/points/");
 
@@ -37,12 +27,11 @@ const PointTake = () => {
 
     const res1J = await res1.json();
 
-    setDriversID(res1J);
-    setUseID(parseInt(driversID.UID));
+    window.localStorage.setItem("DRIVERID", parseInt(res1J.UID));
 
     console.log("changed");
-    url2.searchParams.append("UID", 4);
-    url2.searchParams.append("SID", 1);
+    url2.searchParams.append("UID", window.localStorage.getItem("DRIVERID"));
+    url2.searchParams.append("SID", window.localStorage.getItem("sid"));
     console.log(url2);
 
     const res2 = await fetch(url2, {
@@ -51,20 +40,17 @@ const PointTake = () => {
     });
 
     const res2J = await res2.json();
-    setDriversCurrPoints(res2J);
+    window.localStorage.setItem("POINTSPRE", res2J);
 
-    let curr = parseInt(driversCurrPoints);
-    let chan = parseInt(enteredPointChange);
+    let curr = parseInt(window.localStorage.getItem("POINTSPRE"));
+    let sub = parseInt(window.localStorage.getItem("POINTCHG"));
 
-    console.log(curr + "fucking what" + chan);
-
-    setUsePoints(curr + chan);
-    console.log("The new point amount:" + useThisPoints);
+    window.localStorage.setItem("POINTSPOST", curr + sub);
 
     const stuff = {
-      SID: sid,
-      UID: useThisID,
-      newAmount: useThisPoints,
+      SID: window.localStorage.getItem("sid"),
+      UID: window.localStorage.getItem("DRIVERID"),
+      newAmount: window.localStorage.getItem("POINTSPOST"),
       reason: "good work",
     };
 
@@ -144,16 +130,23 @@ const PointTake = () => {
         <input
           type="text"
           name="Email"
-          value={enteredEmail}
-          onChange={(event) => setEnteredEmail(event.target.value)}
+          value={window.localStorage.getItem("DRIVEREMAIL")}
+          onChange={(event) =>
+            window.localStorage.setItem("DRIVEREMAIL", event.target.value)
+          }
         />
 
         <label>Enter point Change:</label>
         <input
           type="number"
           name="Changa:"
-          value={enteredPointChange}
-          onChange={(event) => setEnteredPointChange(event.target.value)}
+          value={window.localStorage.getItem("POINTCHG")}
+          onChange={(event) =>
+            window.localStorage.setItem(
+              "POINTCHG",
+              parseInt(event.target.value)
+            )
+          }
         />
 
         <input type="submit" />
