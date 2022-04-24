694ffe282d5d8fd3be8e3605c914d3e7165c0c0e
Zach Howes <zhowes14@icloud.com>
2022-04-24 17:36:42 -0400

    created driver password reset - needs fetch

--- front-end/app/src/Components/pages/Driver_Password_Reset.js
+++ front-end/app/src/Components/pages/Driver_Password_Reset.js
@@ -1,26 +1,70 @@
 import React, { useState, useEffect } from "react";
 import DriverNav from "../UI/DriverNav";
 import Footer from "../Footer/Footer";
-import "./Driver_Password_Reset.css"
-import "react-toastify/dist/ReactToastify.css";
-import { toast } from "react-toastify";
-toast.configure();
-
-//onSubmit={submitHandler}
+import "./Driver_Password_Reset.css";
 
 export default function Driver_Password_Reset() {
-    const [enteredEmail, setEnteredEmail] = useState("");
-    return (
-        <div>
-            <DriverNav />
-       
-            <div className = "DReset">
-           
-            </div>
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [reenteredPassword, setReenteredPassword] = useState("");
 
-            <div className="bottom_here">
-                <Footer />
-            </div>
-        </div>
-    )
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    //send call to update password
+    setEnteredPassword("");
+    setReenteredPassword("");
+  };
+
+  return (
+    <div className="page">
+      <DriverNav />
+      <div className="spacer"></div>
+
+      <div>
+        <h1>Password Reset</h1>
+        <h2>Please input your new password and submit</h2>
+      </div>
+      <div className="form-box">
+        <form onSubmit={submitHandler} className="form">
+          <div className="item-box">
+            <label>New Password</label>
+            <p>8-15 character, 1 uppercase, 1 lowercase, 1 special character</p>
+            <input
+              type="password"
+              name="password"
+              value={enteredPassword}
+              placeholder="8-15 Characters"
+              minLength="8"
+              maxLength="15"
+              onChange={(event) => setEnteredPassword(event.target.value)}
+              required
+            />
+          </div>
+          <div className="item-box">
+            <label>Re-Enter New Password</label>
+            <input
+              type="password"
+              value={reenteredPassword}
+              placeholder="8-15 Characters"
+              minLength="8"
+              maxLength="15"
+              onChange={(event) => setReenteredPassword(event.target.value)}
+              required
+            />
+            <hr />
+          </div>
+          <div>
+            <button
+              type="submit"
+              disabled={enteredPassword !== reenteredPassword}
+            >
+              {" "}
+              update password
+            </button>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
 }
